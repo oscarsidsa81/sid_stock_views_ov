365e3040981301d174bfc5e95b76965819a3ec1d
oscarsidsa81 <ovilches@sid-sa.com>
2026-02-20 10:57:57 +0100

    ok

--- __init__.py
+++ __init__.py
@@ -1,5 +1,1 @@
-from . import models
-
-# Exponer el hook en el namespace del paquete para que Odoo pueda resolver
-# post_init_hook="post_init_create_sid_stock_views_ov" vía getattr(py_module, ...)
-from .hooks import post_init_create_sid_stock_views_ov  # noqa: F401
+# -*- coding: utf-8 -*-
--- __manifest__.py
+++ __manifest__.py
@@ -1,24 +1,19 @@
 # -*- coding: utf-8 -*-
 {
-    "name": "SID Stock Views OV",
-    "summary": "Recrea vistas OV de Stock con referencias estables (sin IDs numéricos) y campos SID base.",
-    "version": "15.0.1.0.5",
-    "category": "Inventory",
-    "license": "AGPL-3",
-    "author": "SIDSA",
-    "depends": [
-        "stock",
-        "mail",
-        "sale_stock",
-        "purchase_stock",
-        "sid_product_base",
-        "sid_stock_base",
-        "sid_stock_actions_ov",
+    'name': 'SID Stock Views OV',
+    'summary': 'Vistas OV para Inventario (sin hooks, xml_id estables)',
+    'version': '15.0.1.1.0',
+    'category': 'Inventory/Inventory',
+    'license': 'LGPL-3',
+    'author': 'SIDSA',
+    'depends': [
+        'stock',
+        'sid_stock_legacy',
+        'sid_stock_actions_ov',
     ],
-    "data": [
-        # vistas se crean en post_init para poder localizar inherit_id por nombre
+    'data': [
+        'views/stock_picking_views_ov.xml',
     ],
-    "post_init_hook": "post_init_create_sid_stock_views_ov",
-    "installable": True,
-    "application": False,
+    'installable': True,
+    'application': False,
 }
